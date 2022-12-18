29968a30545ef443fdac6de2b61c65065bcbd468
Natalya Bryzgalova <Natasha.Brizgalova@yandex.rugit config --global user.email Natasha.Brizgalova@yandex.ru>
2022-12-18 23:38:15 +0300

    bag

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__closed');
+const popupOpenButtonElement = document.querySelector('.profile__edit-botton');
+
+const OpenPopup = function() {
+  popupElement.classList.add('popup_is-opened');
+};
+
+const ClosePopup = function() {
+  popupElement.classList.remove('popup_is-opened');
+};
+
+popupOpenButtonElement.addEventListener('click', OpenPopup);
+popupCloseButtonElement.addEventListener('click', ClosePopup);
+
