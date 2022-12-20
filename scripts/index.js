3698814da5847b12f811222005848f53e2e070a0
Natalya Bryzgalova <Natasha.Brizgalova@yandex.rugit config --global user.email Natasha.Brizgalova@yandex.ru>
2022-12-20 14:18:45 +0300

    iteation 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+/* Открытие попап по кнопке Редактировать и закрытие попап по крестику */
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__closed');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
@@ -12,4 +13,29 @@ const ClosePopup = function() {
 
 popupOpenButtonElement.addEventListener('click', OpenPopup);
 popupCloseButtonElement.addEventListener('click', ClosePopup);
+
+
+/* Сохранение на странице введенных данных из формы */
+let formElement = document.querySelector('.popup__form');
+
+let nameInput = formElement.querySelector('.popup__input_name');
+let jobInput = formElement.querySelector('.popup__input_job');
+let saveAndClosePopup = formElement.querySelector('.popup__button');
+
+let profileIntro = document.querySelector('.profile__intro');
+let newProfileTitle = profileIntro.querySelector('.profile__title');
+let newProfileSubtitle = profileIntro.querySelector('.profile__subtitle');
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    nameInput.value;
+    jobInput.value;
+    newProfileTitle.textContent = nameInput.value;
+    newProfileSubtitle.textContent = jobInput.value;
+}
+formElement.addEventListener('submit', handleFormSubmit);
+
+
+/* Закрытие попап по кнопке Сохранить */
+saveAndClosePopup.addEventListener('click', ClosePopup);
 
