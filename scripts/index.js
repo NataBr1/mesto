e5bb52fc3c4b445c4062dfb64d192d70f11740a3
Natalya Bryzgalova <Natasha.Brizgalova@yandex.rugit config --global user.email Natasha.Brizgalova@yandex.ru>
2023-01-22 14:25:50 +0300

    шесть карточек из коробки

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 
 const formElement = document.querySelector('.popup__form');
 
-let nameInput = formElement.querySelector('.popup__input_type_name');
+const nameInput = formElement.querySelector('.popup__input_type_name');
 const jobInput = formElement.querySelector('.popup__input_type_job');
 const saveAndClosePopup = formElement.querySelector('.popup__button');
 
@@ -34,3 +34,56 @@ function handleFormSubmit (evt) {
 
 formElement.addEventListener('submit', handleFormSubmit);
 
+
+/* Массив с карточками */
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+/* Место для вставки template в HTML */
+const elementGrid = document.querySelector('.elements');
+/* Находим template */
+const template = document.querySelector('#cards');
+
+/* Создание и наполнение карточки */
+const createCards = (item) => {
+  const card = template.content.querySelector('.element').cloneNode(true);
+  card.querySelector('.element__photo').src = item.link;
+  card.querySelector('.element__photo').alt = item.name;
+  card.querySelector('.element__title').textContent = item.name;
+
+  return card;
+}
+
+/* Рендеринг карточек */
+const renderCards = (item) => {
+  elementGrid.append(createCards(item))
+};
+
+/* Перебор массива */
+initialCards.forEach((item) => {
+  renderCards(item)
+});
