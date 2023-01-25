b5041b598326803626df93f7f4bce342f80ba838
Natalya Bryzgalova <Natasha.Brizgalova@yandex.rugit config --global user.email Natasha.Brizgalova@yandex.ru>
2023-01-26 00:28:20 +0300

    Итерация 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,62 @@
-const popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = popupElement.querySelector('.popup__closed');
-const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const popupElementUser = document.querySelector('.popup_user'); /* Попап с редактированием профиля */
+const popupElementPlace = document.querySelector('.popup_place') /* Попап добавления карточки */
+const popupCloseButtonElementUser = popupElementUser.querySelector('.popup__closed_user'); /* Кнопка закрытия попапа профиля */
+const popupOpenButtonElementUser = document.querySelector('.profile__edit-button'); /* Кнопка открытия попапа профиля */
+const popupCloseButtonElementPlace = popupElementPlace.querySelector('.popup__closed_place'); /* Кнопка закрытия попапа новой карточки */
+const popupOpenButtonElementPlace = document.querySelector('.profile__add-button'); /* Кнопка открытия попапа новой карточки */
+const popupElementView = document.querySelector('.view'); /* Попап просмотра изображения */
+const popupCloseButtonElementView = popupElementView.querySelector('.popup__closed_view'); /*Кнопка закрытия просмотра изображения*/
 
-const formElement = document.querySelector('.popup__form');
+/* Ищем формы и инпуты */
+const formElementUser = document.querySelector('.popup__form_user');
+const nameUserInput = formElementUser.querySelector('.popup__input_type_name');
+const jobInput = formElementUser.querySelector('.popup__input_type_job');
+const formElementPlace = document.querySelector('.popup__form_place');
+const namePlaceInput = formElementPlace.querySelector('.popup__input_type_name');
+const linkInput = formElementPlace.querySelector('.popup__input_type_link');
 
-const nameInput = formElement.querySelector('.popup__input_type_name');
-const jobInput = formElement.querySelector('.popup__input_type_job');
-const saveAndClosePopup = formElement.querySelector('.popup__button');
-
+/* Ищем отображаемые на странице данные пользователя */
 const profileIntro = document.querySelector('.profile__intro');
 const newProfileTitle = profileIntro.querySelector('.profile__title');
 const newProfileSubtitle = profileIntro.querySelector('.profile__subtitle');
 
-const openPopup = function() {
-  popupElement.classList.add('popup_is-opened');
-  nameInput.value = newProfileTitle.textContent;
+/* Функция открытия/закрытия попапа профиля */
+const openPopupUser = function() {
+  popupElementUser.classList.toggle('popup_opened');
+  nameUserInput.value = newProfileTitle.textContent;
   jobInput.value = newProfileSubtitle.textContent;
 };
 
-const closePopup = function() {
-  popupElement.classList.remove('popup_is-opened');
-};
-
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-    newProfileTitle.textContent = nameInput.value;
-    newProfileSubtitle.textContent = jobInput.value;
-    closePopup();
+/* Функция открытия/закрытия попапа новой карточки */
+const openPopupPlace = function() {
+  popupElementPlace.classList.toggle('popup_opened');
 }
+console.log(1)
 
-formElement.addEventListener('submit', handleFormSubmit);
+/* Функция открыти/закрытия попапа просмотра изображения */
+const openPopupView = function() {
+  popupElementView.classList.toggle('popup_opened');
+}
+console.log(2)
 
+/* Вешаем слушателей на кнопки управления открытием/закрытием попапов */
+popupOpenButtonElementUser.addEventListener('click', openPopupUser);
+popupCloseButtonElementUser.addEventListener('click', openPopupUser);
+popupOpenButtonElementPlace.addEventListener('click', openPopupPlace);
+popupCloseButtonElementPlace.addEventListener('click', openPopupPlace);
+popupCloseButtonElementView.addEventListener('click', openPopupView);
+console.log(3)
 
+/* Сохранение новых данных о пользователе на странице */
+function handleFormUserSubmit (evt) {
+    evt.preventDefault();
+    newProfileTitle.textContent = nameUserInput.value;
+    newProfileSubtitle.textContent = jobInput.value;
+    popupElementUser.classList.remove('popup_opened');
+}
+formElementUser.addEventListener('submit', handleFormUserSubmit);
+
+console.log(4)
 /* Массив с карточками */
 const initialCards = [
   {
@@ -68,22 +90,57 @@ const elementGrid = document.querySelector('.elements');
 /* Находим template */
 const template = document.querySelector('#cards');
 
-/* Создание и наполнение карточки */
+/* Отображение карточек из массива */
 const createCards = (item) => {
   const card = template.content.querySelector('.element').cloneNode(true);
-  card.querySelector('.element__photo').src = item.link;
-  card.querySelector('.element__photo').alt = item.name;
-  card.querySelector('.element__title').textContent = item.name;
+  const namePlace = card.querySelector('.element__title');
+  namePlace.textContent = item.name;
+  const linkPlace = card.querySelector('.element__photo');
+  linkPlace.src = item.link;
+  const attributeAltPlace = card.querySelector('.element__photo');
+  attributeAltPlace.alt = item.name;
+console.log(5)
+  /* Лайк */
+  card.querySelector('.element__like').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('element__like_active');
+  });
 
+  /* Удаление карточек */
+  card.querySelector('.element__delete').addEventListener('click', function () {
+    card.remove();
+  })
+console.log(6)
+  /*Просмотр изображения*/
+ linkPlace.addEventListener('click', function() {
+    openPopupView(item);
+    popupElementView.querySelector('.view__caption').textContent = item.name;
+    popupElementView.querySelector('.view__photo').src = item.link;
+    popupElementView.querySelector('.view__photo').alt = item.name;
+  });
+console.log(7)
   return card;
-}
+};
 
 /* Рендеринг карточек */
 const renderCards = (item) => {
-  elementGrid.append(createCards(item))
+  elementGrid.append(createCards(item));
 };
 
-/* Перебор массива */
+/* Перебор массива с карточками*/
 initialCards.forEach((item) => {
-  renderCards(item)
+  renderCards(item);
 });
+
+/* Добавление новой карточки */
+function handleFormPLaceSubmit (evt) {
+  evt.preventDefault();
+  elementGrid.prepend(createCards(
+    {
+      name: namePlaceInput.value,
+      link: linkInput.value
+    }));
+
+  popupElementPlace.classList.remove('popup_opened');
+};
+
+formElementPlace.addEventListener('submit', handleFormPLaceSubmit);
